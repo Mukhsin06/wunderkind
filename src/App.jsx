eb3735bc5bcd05c6797ab77c60841975bb4e4fd4
abdukarimov0990 <oyatbek09@gmail.com>
2025-05-15 23:30:28 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router';
+import { RouterProvider } from 'react-router-dom';
 
 function App(props) {
+    const routes = createBrowserRouter(
+        createRoutesFromElements(
+          <Route element={<MainLayout/>}>
+            <Route path='/' element={<Home/>}></Route>
+          </Route>
+        )
+      )
+    
     return (
-        <div >
+        <RouterProvider router={routes}/>
 
-
-        </div>
     );
 }
 
